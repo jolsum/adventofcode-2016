7ec4b2d56c24e7456a59f50e5d9db60019493812
Lars Jølsum <github@jolsum.net>
2016-12-07 23:49:24 +0100

    Day2 complete

--- src/lars/adventofcode/Day2.java
+++ src/lars/adventofcode/Day2.java
@@ -7,54 +7,67 @@ import java.util.Arrays;
 
 public class Day2 {
 
-    public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException {
 
-	int rows = 3;
-	int columns = 3;
+		int[][] keyPad = {
+				{ 0, 0, 1, 0, 0 },
+				{ 0, 2, 3, 4, 0 },
+				{ 5, 6, 7, 8, 9 },
+				{ 0, 0xA, 0xB, 0xC, 0 },
+				{ 0, 0, 0xD, 0, 0 }
+		};
 
-	int[][] keyPad = new int[rows][columns];
+		// int[][] keyPad = {
+		// { 1, 2, 3 },
+		// { 4, 5, 6 },
+		// { 7, 8, 9 }
+		// };
 
-	int startX = 0;
-	int startY = 0;
+		int startX = 0;
+		int startY = 0;
 
-	int num = 0;
-	for (int i = 0; i < rows; i++) {
-	    for (int j = 0; j < columns; j++) {
-		keyPad[i][j] = ++num;
-		if (num == 5) {
-		    startX = i;
-		    startY = j;
+		for (int i = 0; i < keyPad.length; i++) {
+			for (int j = 0; j < keyPad[0].length; j++) {
+				if (keyPad[i][j] == 5) {
+					startX = i;
+					startY = j;
+				}
+			}
+			System.out.println(Arrays.toString(keyPad[i]));
 		}
-	    }
-	    System.out.println(Arrays.toString(keyPad[i]));
-	}
-	int posX = startX;
-	int posY = startY;
+		int posX = startX;
+		int posY = startY;
 
-	for (String line : Files.readAllLines(Paths.get("input", "2.txt"))) {
+		for (String line : Files.readAllLines(Paths.get("input", "2.txt"))) {
 
-	    for (int i = 0; i < line.length(); i++) {
-		char a = line.charAt(i);
-		if (a == 'R') {
-		    posY++;
-		} else if (a == 'L') {
-		    posY--;
-		} else if (a == 'U') {
-		    posX--;
-		} else if (a == 'D') {
-		    posX++;
-		} else {
-		    throw new IllegalArgumentException(Character.toString(a));
+			for (int i = 0; i < line.length(); i++) {
+
+				int newX = posX;
+				int newY = posY;
+
+				char a = line.charAt(i);
+				if (a == 'R') {
+					newY++;
+				} else if (a == 'L') {
+					newY--;
+				} else if (a == 'U') {
+					newX--;
+				} else if (a == 'D') {
+					newX++;
+				}
+
+				if (newX >= 0 && newX < keyPad.length &&
+						newY >= 0 && newY < keyPad[0].length &&
+						keyPad[newX][newY] != 0) {
+					posX = newX;
+					posY = newY;
+				}
+
+			}
+
+			System.out.print(keyPad[posX][posY] + " ");
 		}
 
-		posX = Math.max(0, Math.min(posX, rows - 1));
-		posY = Math.max(0, Math.min(posY, columns - 1));
-
-	    }
-
-	    System.out.print(keyPad[posX][posY]);
 	}
-
-    }
 
 }
