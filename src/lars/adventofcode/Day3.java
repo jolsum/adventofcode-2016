478a2b37d88097c5275bcd30c917f68740ed0ceb
Lars Jølsum <github@jolsum.net>
2016-12-08 11:21:30 +0100

    Day 3 complete

--- src/lars/adventofcode/Day3.java
+++ src/lars/adventofcode/Day3.java
@@ -3,19 +3,42 @@ package lars.adventofcode;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Day3 {
 
 	public static void main(String[] args) throws IOException {
 
-		long count = Files.readAllLines(Paths.get("input", "3.txt"))
-				.stream()
+		List<String> lines = Files.readAllLines(Paths.get("input", "3.txt"));
+
+		long part1 = lines.stream()
 				.map(Day3::toTriangle)
 				.filter(Day3::isValid)
 				.count();
 
-		System.out.println(count);
+		System.out.println("Part 1: " + part1);
 
+		long part2 = makeTriangles(lines).stream()
+				.filter(Day3::isValid)
+				.count();
+
+		System.out.println("Part 2: " + part2);
+	}
+
+	private static List<Triangle> makeTriangles(List<String> lines) {
+
+		List<Triangle> res = new ArrayList<>();
+		for (int i = 0; i < lines.size();) {
+			Triangle t1 = toTriangle(lines.get(i++));
+			Triangle t2 = toTriangle(lines.get(i++));
+			Triangle t3 = toTriangle(lines.get(i++));
+
+			res.add(new Triangle(t1.a, t2.a, t3.a));
+			res.add(new Triangle(t1.b, t2.b, t3.b));
+			res.add(new Triangle(t1.c, t2.c, t3.c));
+		}
+		return res;
 	}
 
 	private static boolean isValid(Triangle t) {
@@ -29,17 +52,23 @@ public class Day3 {
 				.replaceAll("  ", " ")
 				.split(" ");
 
-		Triangle t = new Triangle();
-		t.a = Integer.parseInt(parts[0]);
-		t.b = Integer.parseInt(parts[1]);
-		t.c = Integer.parseInt(parts[2]);
-		return t;
+		return new Triangle(
+				Integer.parseInt(parts[0]),
+				Integer.parseInt(parts[1]),
+				Integer.parseInt(parts[2]));
 	}
 
 	private static class Triangle {
 		int a;
 		int b;
 		int c;
+
+		public Triangle(int a, int b, int c) {
+			this.a = a;
+			this.b = b;
+			this.c = c;
+		}
+
 	}
 
 }
