0c881b7f45fdaa6d2beeb59a010e1a83505fa374
Lars Jølsum <github@jolsum.net>
2016-12-14 23:08:30 +0100

    Day 10 complete

--- src/lars/adventofcode/Day10.java
+++ src/lars/adventofcode/Day10.java
@@ -3,18 +3,57 @@ package lars.adventofcode;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.HashSet;
+import java.util.HashMap;
 import java.util.List;
-import java.util.Set;
+import java.util.Map;
 import java.util.SortedSet;
 import java.util.TreeSet;
 
 public class Day10 {
 
+	private static final Map<Integer, Bot> bots = new HashMap<>();
+	private static final Map<Integer, Output> outputs = new HashMap<>();
+
 	public static void main(String[] args) throws IOException {
 
-		List<String> lines = Files.readAllLines(Paths.get("input", "10_test.txt"));
+		List<String> lines = Files.readAllLines(Paths.get("input", "10.txt"));
 
+		for (String line : lines) {
+			String[] parts = line.split(" ");
+			if (line.startsWith("bot")) {
+				int botId = Integer.parseInt(parts[1]);
+				Bot bot = bots.computeIfAbsent(botId, Bot::new);
+				bot.lowTarget = getReceiver(parts[5], parts[6]);
+				bot.highTarget = getReceiver(parts[10], parts[11]);
+			}
+		}
+
+		for (String line : lines) {
+			String[] parts = line.split(" ");
+			if (line.startsWith("value")) {
+				int chip = Integer.parseInt(parts[1]);
+				int bot = Integer.parseInt(parts[5]);
+				bots.computeIfAbsent(bot, Bot::new).receive(chip);
+			}
+		}
+
+		int product = outputs.get(0).chips.first() *
+				outputs.get(1).chips.first() *
+				outputs.get(2).chips.first();
+
+		System.out.println("Part 2: " + product);
+	}
+
+	private static ChipReceiver getReceiver(String botOrOutput, String num) {
+		int id = Integer.parseInt(num);
+
+		if ("bot".equals(botOrOutput)) {
+			return bots.computeIfAbsent(id, Bot::new);
+		}
+		else if ("output".equals(botOrOutput)) {
+			return outputs.computeIfAbsent(id, Output::new);
+		}
+		throw new IllegalArgumentException("Unknown receiver type: " + botOrOutput);
 	}
 
 	private static abstract class ChipReceiver {
@@ -28,6 +67,10 @@ public class Day10 {
 
 		abstract void receive(Integer chip);
 
+		@Override
+		public String toString() {
+			return getClass().getSimpleName() + " " + id;
+		}
 	}
 
 	private static class Bot extends ChipReceiver {
@@ -42,11 +85,20 @@ public class Day10 {
 		@Override
 		public void receive(Integer chip) {
 			chips.add(chip);
+
 			if (chips.size() == 2) {
-				lowTarget.receive(chips.first());
-				highTarget.receive(chips.last());
+				if (chips.first() == 17 && chips.last() == 61) {
+					System.out.println("Part 1: " + id);
+				}
+
+				give(chips.first(), lowTarget);
+				give(chips.last(), highTarget);
 			}
-			chips.clear();
+		}
+
+		private void give(Integer chip, ChipReceiver receiver) {
+			chips.remove(chip);
+			receiver.receive(chip);
 		}
 
 	}
@@ -56,7 +108,7 @@ public class Day10 {
 		public Output(int id) {
 			super(id);
 		}
-		
+
 		@Override
 		public void receive(Integer chip) {
 			chips.add(chip);
